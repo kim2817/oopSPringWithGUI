044af6062ff0053b2cad61f6c79d3447d94df20d
eyad-dev <24P0240@eng.asu.edu.eg>
2025-05-06 01:50:17 +0300

    Eyad's Admin and

---
+++ src/main/java/frontend/AdminInterface.java
@@ -0,0 +1,95 @@
+package frontend;
+
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.text.Font;
+import javafx.util.Duration;
+
+import java.awt.*;
+
+public class AdminInterface extends Application {
+    public void start(Stage stage){
+        stage.setTitle("Admin Interface");
+        stage.getIcons().add(new Image("img.png"));
+
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+
+        VBox sidebar = new VBox();
+        sidebar.setStyle("-fx-background-color: #333; -fx-padding: 10;");
+        sidebar.setPrefWidth(200);
+        stage.setResizable(false);
+
+        Button toggleButton = new Button("☰");
+
+        toggleButton.setStyle("-fx-font-size: 16;");
+        grid.add(toggleButton,0,1);
+        grid.add(sidebar,0,0 );
+
+        toggleButton.setOnAction(e ->{
+            double targetX = sidebar.getTranslateX()==-250? 600: -250;
+
+            TranslateTransition transition = new TranslateTransition(Duration.millis(300), sidebar);
+            transition.setToX(targetX);
+            transition.play();
+
+        });
+
+
+
+
+        Button accDetails = new Button("View Account");
+        Button logout = new Button("Log out");
+
+        sidebar.getChildren().addAll(accDetails, logout);
+
+        sidebar.setTranslateX(600);
+        Scene scene = new Scene(grid, 600, 400);
+
+        Text greeting = new Text("Hello Mr/Mrs: Admin");
+        greeting.setFont(Font.font("Arial"));
+        Line line = new Line();
+
+//        line.setStartX(0);
+//        line.setEndX(2000);
+//        line.setStroke(Color.BLACK);
+        Button eventsBtn = new Button("Events");
+        Button usersBtn = new Button("Users");
+        Button mngDataBtn = new Button("Manage Data");
+
+
+        eventsBtn.setOnAction(e->){
+
+        };
+        grid.add(greeting,1,1);
+        grid.add(eventsBtn, 0,4);
+        grid.add(usersBtn, 0,5);
+        grid.add(mngDataBtn, 0,6);
+        stage.setScene(scene);
+        stage.show();
+
+        ScrollPane scrollPane = new ScrollPane(grid);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene1 = new Scene(scrollPane, 500, 700);
+        grid.setPadding(new Insets(20));
+
+    }
+
+
+}
