620a4668bc9479abd6f7d3969772dbcab24d226c
Jasmin <24P0225@eng.asu.edu.eg>
2025-05-06 03:43:08 +0300

    parsing and removing interface

--- src/main/java/BackEnd/Entrance.java
+++ src/main/java/BackEnd/Entrance.java
@@ -6,15 +6,16 @@ public class Entrance {
 
 
     public static void registerOrganizer(String email, String username, String password,
-                                         String DOB,boolean gender,double walletBalance){
+                                         String DOB,boolean gender,String walletBalance){
         Gender gen= gender?Gender.FEMALE:Gender.MALE;
-        Database.create(new Organizer(email,username,password,new DateTime(DOB),gen,walletBalance));
+
+        Database.create(new Organizer(email,username,password,new DateTime(DOB),gen,Double.parseDouble(walletBalance)));
     }
 
     public static void registerAttendee(String email, String username, String password,
-                                        String DOB,boolean gender,int age, String address, double walletBalance){
+                                        String DOB,boolean gender,String age, String address, String walletBalance){
         Gender gen= gender?Gender.FEMALE:Gender.MALE;
-        Database.create(new Attendee(email, username, password, new DateTime(DOB),gen, age, address, walletBalance));
+        Database.create(new Attendee(email, username, password, new DateTime(DOB),gen, Integer.parseInt(age), address,Double.parseDouble(walletBalance)));
     }
 
     public static int login(String username, String password){
