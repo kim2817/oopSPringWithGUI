606c98de4c4a9ae38cb6a81aba399efa154bfc8f
kim28 <karma.adnan.hussein@gmail.com>
2025-05-06 06:05:27 +0300

    ok

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -19,8 +19,6 @@ public class Main extends Application{
 
     public static void main(String[] args) {
         Database.create(new Attendee("j", "j", "123", new DateTime("16/02/2008"),FEMALE, Integer.parseInt("17"),
-                "address",Double.parseDouble("500")));
-        Database.create(new Attendee("j", "j", "123", new DateTime("16/02/2008"),FEMALE, Integer.parseInt("17"),
                 "address",Double.parseDouble("50.5"), "standup comedy", "plays","workshops"));
         Database.create(new Organizer("k","k","123",new DateTime("28/11/2007"),FEMALE,Double.parseDouble("550.6")));
         Database.create(new Admin("e","e","123", new DateTime("01/01/0001"), Gender.MALE, "role", "9 to 5"));
