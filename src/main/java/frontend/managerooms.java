d1a51a6503a41d73db3a87a6a135869f59a71e0a
eyad-dev <24P0240@eng.asu.edu.eg>
2025-05-06 03:52:16 +0300

    Eyad's Admin and stuff

--- src/main/java/frontend/managerooms.java
+++ src/main/java/frontend/managerooms.java
@@ -4,22 +4,28 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class managerooms{
     public static void show(){
         Stage stage = new Stage();
-        FlowPane pane = new FlowPane();
-        pane.setPadding(new Insets(10));
+        HBox pane = new HBox(100);
         Scene scene = new Scene(pane, 300, 300);
+        ToggleGroup group = new ToggleGroup();
         RadioButton roomChoice = new RadioButton("Room");
         RadioButton catChoice = new RadioButton("Category");
 
+
+
+        pane.setPadding(new Insets(20));
         pane.getChildren().addAll(roomChoice, catChoice);
         stage.setScene(scene);
         stage.show();
+
     }
 
 
