6068c7fee03a25e0d8b7c1769390885e547a5419
kim28 <karma.adnan.hussein@gmail.com>
2025-05-06 04:21:34 +0300

    ok

--- src/main/java/frontend/AttendeeGUI.java
+++ src/main/java/frontend/AttendeeGUI.java
@@ -5,23 +5,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class AttendeeGUI extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-    @Override
-    public void start(Stage stage) throws Exception {
+public class AttendeeGUI {
+    public static void show(String username, double balance) {
+        Stage stage = new Stage();
         stage.setTitle("Eventra - Attendee Dashboard");
 
-        //profile pic
-        Image profileImage = new Image("profile.png"); // Use actual path or file
-        ImageView profileIcon = new ImageView(profileImage);
-        profileIcon.setFitWidth(40);
-        profileIcon.setFitHeight(40);
 
-
-
-
+    }
 
     }
 }
