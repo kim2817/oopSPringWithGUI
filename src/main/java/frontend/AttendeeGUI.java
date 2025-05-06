edc08e48f4b5f0f77fe246ec0cab1293d14eb910
kim28 <karma.adnan.hussein@gmail.com>
2025-05-06 04:24:15 +0300

    ok

--- src/main/java/frontend/AttendeeGUI.java
+++ src/main/java/frontend/AttendeeGUI.java
@@ -6,7 +6,7 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class AttendeeGUI {
-    public static void show(String username, double balance) {
+    public static void show(String username, double balance, String ID) {
         Stage stage = new Stage();
         stage.setTitle("Eventra - Attendee Dashboard");
 
