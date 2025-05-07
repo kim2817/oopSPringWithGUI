e01b5dc9c5dcb757958b8a4f54f71b39972860d4
Yahia_g <yahiaahmed2yahia@hotmail.com>
2025-05-07 18:42:46 +0300

    more uis for organizer

--- src/main/java/frontend/OraganizerUI.java
+++ src/main/java/frontend/OraganizerUI.java
@@ -1,15 +1,18 @@
 package frontend;
 
+
 import BackEnd.*;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
 
@@ -17,6 +20,7 @@ import javafx.scene.text.Font;
 
 
 class OrganizerUI {
+    static Organizer sessionOrg;
     static Label lbl(String s , double top , double left){
 
         Label Dname = new Label(s);
@@ -44,27 +48,41 @@ class OrganizerUI {
 
 
     public static void show(Organizer u) {
+        sessionOrg = u;
+
         Stage stage = new Stage();
-        stage.setResizable(false);
-        AnchorPane layout = new AnchorPane();
-        Scene s = new Scene(layout , 500,250);
+        HBox layoutx1 = new HBox(10);
+        VBox layouty1 = new VBox(10, layoutx1);
+        HBox layoutx2 = new HBox(10);
+        VBox layouty2 = new VBox(10, layoutx2);
+        HBox layoutx3 = new HBox(10);
+        VBox layouty3 = new VBox(10, layoutx3);
+
+        VBox layout = new VBox(layouty1,layouty2,layouty3);
+        Scene s = new Scene(layout , 800,450);
         stage.setScene(s);
-        String name = "Hello Mr/Ms: " + u.getUsername();
 
-        layout.setPadding(new Insets(10));
+        layout.setPadding(new Insets(20));
 
-        layout.getChildren().add(lbl(name,10.0,10.0));
-        layout.getChildren().add(lbl("Balance: " + u.getBalance().getBalance(),30.0, 10.0 ));
-        layout.getChildren().add(lbl("--------------------------------------------------------",70.0, 100.0 ));
+        layouty1.getChildren().add(lbl("Hello Mr/Ms: " + u.getUsername(),10.0,10.0));
+        layouty1.getChildren().add(lbl("Balance: " + u.getBalance().getBalance(),30.0, 10.0 ));
+        layouty1.setAlignment(Pos.TOP_LEFT);
+        layouty2.getChildren().add(lbl("----------------------------------------------------------------------------------------------------------------------------------------------------------",70.0, 100.0 ));
+        layouty2.setAlignment(Pos.CENTER);
         Button viewEvents = btn("View Events",90.0,100.0);
         Button myEvents = btn("My Events",90.0,300.0);
         Button logout = btn("Logout",150.0,210.0);
-        layout.getChildren().addAll(viewEvents,myEvents,logout);
+        layoutx3.getChildren().addAll(viewEvents,myEvents,logout);
+        layoutx3.setAlignment(Pos.CENTER);
         stage.show();
 
-
         //viewEvents.setOnAction(e->);
-        //myEvents.setOnAction(e->);
+        myEvents.setOnAction(e->{
+            stage.close();
+            MyEventsUI.show(u);
+
+
+        });
         logout.setOnAction(e-> {
             stage.close();
             LoginWindow.show();
@@ -73,73 +91,258 @@ class OrganizerUI {
 }
 
 class ViewEventsUI {
+    public static void show(){
 
+
+
+
+
+
+    }
 
 
 }
 
 class MyEventsUI {
+    public static void show(Organizer u){
+        Stage stage = new Stage();
+        HBox layoutx1 = new HBox(10);
+        VBox layouty1 = new VBox(10, layoutx1);
+        VBox layouty2 = new VBox(10);
+        HBox layoutx2 = new HBox(10,layouty2);
+        HBox layoutx3 = new HBox(10);
+        VBox layouty3 = new VBox(10, layoutx3);
 
+        VBox layout = new VBox(layouty1,layoutx2,layouty3);
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
+
+        layout.setPadding(new Insets(20));
+
+        ObservableList<String> items = FXCollections.observableArrayList(Category.listAllCategories());
+        ComboBox<String> CatsCombo1 = new ComboBox<>();
+        CatsCombo1.setPromptText("Select Category");
+        CatsCombo1.getItems().addAll(items);
+        layouty1.getChildren().addAll(new Label("My Events"),CatsCombo1);
+        layouty1.setAlignment(Pos.TOP_LEFT);
+
+        Button createNewEvent = new Button("Create New Event");
+        Button viewOrganisedEvents = new Button("View Organised Events");
+        Button manageEvent = new Button("Manage Events");
+        layoutx2.getChildren().addAll(createNewEvent,viewOrganisedEvents,manageEvent);
+        layoutx2.setAlignment(Pos.CENTER);
+
+        createNewEvent.setOnAction(e->{
+            stage.close();
+            CreateNewEventUI.show(u);
+        });
+
+        stage.show();
+
+
+
+
+
+    }
 
 
 }
 
 class CreateNewEventUI {
+    public static void show(Organizer u){
 
+        Stage stage = new Stage();
+        VBox layouty1 = new VBox(10);
+        HBox layoutx1 = new HBox(10);
+        VBox layouty2 = new VBox(10);
+        HBox layoutx2 = new HBox(10);
+        HBox layoutx3 = new HBox(10);
+        VBox layouty3 = new VBox(10, layoutx3);
+        HBox layoutx4 = new HBox(10);
+        VBox layouty4 = new VBox(10, layoutx4);
+
+
+
+        VBox layout = new VBox(layoutx1,layoutx2,layouty3,layouty4);
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
+
+        layout.setPadding(new Insets(20));
+
+        TextField nameField = new TextField();
+        layoutx1.getChildren().addAll(new Label("Name     "),nameField);
+        layoutx1.setAlignment(Pos.TOP_LEFT);
+
+        TextField catField = new TextField();
+        layoutx2.getChildren().addAll(new Label("Category"),catField);
+        layoutx2.setAlignment(Pos.TOP_LEFT);
+
+        layouty3.getChildren().addAll(layoutx1,layoutx2);
+
+        Button rentRoom = new Button("Rent Room");
+        layouty4.getChildren().addAll(rentRoom);
+        layouty4.setAlignment(Pos.BOTTOM_LEFT);
+
+        stage.show();
+
+
+
+
+
+    }
 
 
 }
 
 class RentRoomUI {
+    public static void show(){
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        AnchorPane layout = new AnchorPane();
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
 
+
+
+
+
+    }
 
 
 }
 
 class RentRoomDetailsUI {
+    public static void show(){
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        AnchorPane layout = new AnchorPane();
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
 
+
+
+
+
+    }
 
 
 }
 
 class RentRoomConfirmationUI {
+    public static void show(){
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        AnchorPane layout = new AnchorPane();
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
 
+
+
+
+
+    }
 
 
 }
 
 class ManageOrganizedEventUI {
+    public static void show(){
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        AnchorPane layout = new AnchorPane();
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
 
+
+
+
+
+    }
 
 
 }
 
 class EditEventDetailsUI {
+    public static void show(){
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        AnchorPane layout = new AnchorPane();
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
 
+
+
+
+
+    }
 
 
 }
 
 class DeleteEventConfirmationUI {
+    public static void show(){
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        AnchorPane layout = new AnchorPane();
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
 
+
+
+
+
+    }
 
 
 }
 
 class EventSearchResultUI {
+    public static void show(){
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        AnchorPane layout = new AnchorPane();
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
 
+
+
+
+
+    }
 
 
 }
 
 class EventFilterResultUI {
+    public static void show(){
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        AnchorPane layout = new AnchorPane();
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
 
+
+
+
+
+    }
 
 
 }
 
 class ViewEventDetailsUI {
+    public static void show(){
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        AnchorPane layout = new AnchorPane();
+        Scene s = new Scene(layout , 800,450);
+        stage.setScene(s);
 
+
+
+
+
+    }
 
 
 }
